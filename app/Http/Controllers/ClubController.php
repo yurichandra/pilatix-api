61b570fac39bb8f4d1ebe0ba13dc234a6d95b06d
Yuri Chandra <yurichandra@gmail.com>
2019-05-07 22:55:52 +0700

    [WIP] Get tickets by a club ID

--- app/Http/Controllers/ClubController.php
+++ app/Http/Controllers/ClubController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Transformers\ClubTransformer;
 use App\Models\Club;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ClubController extends RestController
 {
@@ -20,6 +21,35 @@ class ClubController extends RestController
             $collection = $this->generateCollection(Club::get());
 
             return $this->sendResponse($collection);
+        } catch (\Exception $e) {
+            return $this->sendIseResponse($e->getMessage());
+        }
+    }
+
+    /**
+     * Get all tickets by club ID.
+     *
+     * @param int $id
+     * @return void
+     */
+    public function getTicketsByClub($id)
+    {
+        try {
+            $clubs = Club::findOrFail($id);
+
+            $home_match_tickets = $clubs->homeMatches
+                ->map(function ($item) {
+                    return $item->tickets;
+                });
+            
+            $away_match_tickets = $clubs->awayMatches
+                ->map(function ($item) {
+                    return $item->tickets;
+                });
+
+            
+        } catch (ModelNotFoundException $e) {
+            return $this->sendNotFoundResponse($e->getMessage());
         } catch (\Exception $e) {
             return $this->sendIseResponse($e->getMessage());
         }
