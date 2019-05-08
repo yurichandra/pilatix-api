a1c42db25dc70dba21398ee6b05a1e82fc728301
Yuri Chandra <yurichandra@gmail.com>
2019-05-08 20:25:05 +0700

    Adds TicketTransformer class

---
+++ app/Transformers/TicketTransformer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use App\Models\Ticket;
+
+class TicketTransformer extends TransformerAbstract
+{
+    protected $defaultIncludes = [
+        'match',
+        'type',
+        'category',
+    ];
+
+    public function transform(Ticket $ticket)
+    {
+        return [
+            'id' => $ticket->id,
+        ];
+    }
+
+    public function includeMatch(Ticket $ticket)
+    {
+        return $this->item($ticket->match, new MatchTransformer);
+    }
+
+    public function includeType(Ticket $ticket)
+    {
+        return $this->item($ticket->type, new TypeTransformer);
+    }
+
+    public function includeCategory(Ticket $ticket)
+    {
+        return $this->item($ticket->category, new CategoryTransformer);
+    }
+}
