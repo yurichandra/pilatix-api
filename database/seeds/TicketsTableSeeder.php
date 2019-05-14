87aae4ab8ef047737255eaf764da0567e97df099
Yuri Chandra <yurichandra@gmail.com>
2019-05-14 20:48:52 +0700

    Update by adding `price` attributes

--- database/seeds/TicketsTableSeeder.php
+++ database/seeds/TicketsTableSeeder.php
@@ -11,24 +11,28 @@ class TicketsTableSeeder extends Seeder
             'match_id' => 1,
             'category_id' => 2,
             'type_id' => 2,
+            'price' => 150000,
         ],
         [
             'id' => 2,
             'match_id' => 2,
             'category_id' => 2,
             'type_id' => 2,
+            'price' => 75000,
         ],
         [
             'id' => 3,
             'match_id' => 3,
             'category_id' => 2,
             'type_id' => 2,
+            'price' => 80000,
         ],
         [
             'id' => 4,
             'match_id' => 4,
             'category_id' => 2,
             'type_id' => 2,
+            'price' => 50000,
         ],
     ];
 
