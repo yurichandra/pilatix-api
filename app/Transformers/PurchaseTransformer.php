923de13e167303906ce0e5489a5aefb1aef9d52a
Yuri Chandra <yurichandra@gmail.com>
2019-05-14 20:51:09 +0700

    Add PurchaseTransformer to transform Purchase model

---
+++ app/Transformers/PurchaseTransformer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use App\Models\Purchase;
+
+class PurchaseTransformer extends TransformerAbstract
+{
+    /**
+     * Add default includes of model
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        'details',
+    ];
+
+    /**
+     * Transform the model
+     *
+     * @param Purchase $purchase
+     * @return void
+     */
+    public function transform(Purchase $purchase)
+    {
+        return [
+            'id' => $purchase->id,
+            'date' => $purchase->date,
+            'total' => $purchase->total,
+        ];
+    }
+
+    /**
+     * Including detail models.
+     *
+     * @param Purchase $purchase
+     * @return void
+     */
+    public function includeDetails(Purchase $purchase)
+    {
+        return $this->collection($purchase->details, new PurchaseDetailTransformer);
+    }
+}
