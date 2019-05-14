43a9197bf6eb82c1790dcb40bd416bd8b7fa904b
Yuri Chandra <yurichandra@gmail.com>
2019-05-14 20:45:54 +0700

    Add Purchase model

---
+++ app/Models/Purchase.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Purchase extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * Attributes that mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'paid',
+        'date',
+        'total',
+    ];
+
+    /**
+     * Cast an dates attribute.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Cast attributes.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'total' => 'double',
+    ];
+
+    /**
+     * Define relation with PurchaseDetail
+     *
+     * @return void
+     */
+    public function details()
+    {
+        return $this->hasMany(PurchaseDetail::class, 'purchase_id');
+    }
+}
