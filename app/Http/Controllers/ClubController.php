0eae95f91e7f572a3d8f265d1aa46b689e441f95
Yuri Chandra <yurichandra@gmail.com>
2019-05-22 03:33:40 +0700

    Update methods to get tickets base on club id

--- app/Http/Controllers/ClubController.php
+++ app/Http/Controllers/ClubController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Transformers\ClubTransformer;
 use App\Models\Club;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Models\Ticket;
+use App\Transformers\TicketTransformer;
 
 class ClubController extends RestController
 {
@@ -37,17 +39,26 @@ class ClubController extends RestController
         try {
             $clubs = Club::findOrFail($id);
 
-            $home_match_tickets = $clubs->homeMatches
+            $home_match_ids = $clubs->homeMatches
                 ->map(function ($item) {
-                    return $item->tickets;
+                    return $item->id;
                 });
             
-            $away_match_tickets = $clubs->awayMatches
+            $away_match_ids = $clubs->awayMatches
                 ->map(function ($item) {
-                    return $item->tickets;
+                    return $item->id;
                 });
 
-            
+            $home_match_ids->merge($away_match_ids);
+
+            $tickets = collect($home_match_ids)
+                ->map(function ($item) {
+                    return Ticket::where('match_id', $item)->first();
+                });
+
+            $collection = $this->generateCollection($tickets, new TicketTransformer);
+
+            return $this->sendResponse($collection);
         } catch (ModelNotFoundException $e) {
             return $this->sendNotFoundResponse($e->getMessage());
         } catch (\Exception $e) {
